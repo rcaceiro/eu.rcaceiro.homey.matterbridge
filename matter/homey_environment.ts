3755edc991b7d1145b5be8a5dfc54a3c15683522
Rúben Caceiro <caceiro.ruben@gmail.com>
2024-11-02 12:52:54 +0000

    Support debug mode

--- matter/homey_environment.ts
+++ matter/homey_environment.ts
@@ -1,17 +1,23 @@
-import Homey from "homey";
-import {StorageBackendMemory, StorageService} from "@matter/main";
+import {Logger, LogLevel, StorageBackendMemory, StorageService} from "@matter/main";
 import {Environment} from "@matter/general";
+import {StorageBackendDiskAsync} from "@matter/nodejs";
 
 export class HomeyEnvironment extends Environment {
-    private readonly app: Homey.App
-
-    constructor(app: Homey.App, environment: Environment = Environment.default) {
+    constructor(debug: boolean = false, environment: Environment = Environment.default) {
         super("homey_environment", environment);
-        this.app = app;
 
         let storage = new StorageService(this, () => {
-            return new StorageBackendMemory()
+            if (debug) {
+                return new StorageBackendMemory({})
+            }
+            return new StorageBackendDiskAsync("/userdata/eu.rcaceiro.homey.matterbridge/matter_rc")
         });
         this.set(StorageService, storage)
+
+        if (debug) {
+            Logger.defaultLogLevel = LogLevel.DEBUG;
+        } else {
+            Logger.defaultLogLevel = LogLevel.WARN;
+        }
     }
 }
